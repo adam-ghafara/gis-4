5d0558cc716c14b1e08c02229a6ac31982821c3e
adam-ghafara <adam.ghafara.job@gmail.com>
2024-02-06 13:56:31 +0700

    Baru Lagi

--- js/config/configpeta.js
+++ js/config/configpeta.js
@@ -8,7 +8,7 @@ import {container} from 'https://jscroot.github.io/element/croot.js';
 
 const attributions = '<a href="https://petapedia.github.io/" target="_blank">&copy; PetaPedia Indonesia</a> ';
 
-const place = [107.59435735927934, -6.945957168378598];
+const place = [107.59206939350383, -6.945957168378598];
 
 export let idmarker = {id:1};
 
